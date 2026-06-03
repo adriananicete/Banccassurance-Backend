8951782773924261e81128d69bf0c227a5bdd669
PL-johndarrylc <phillifedevops@gmail.com>
2026-06-03 16:30:56 +0800

    Changed the Send Consent message

--- hash.js
+++ hash.js
@@ -3,5 +3,5 @@ import bcrypt from 'bcrypt';
 const password = '123456'; //test password
 
 bcrypt.hash(password, 10).then(hash => {
- console.log('Hashed password:', hash);
+    console.log('Hashed password:', hash);
 });
