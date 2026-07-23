bef7acee7f34349e031d5167fc26865e27b914f4
adriananicete <anicete.ian14@gmail.com>
2026-07-23 10:06:01 +0800

    add branch name in jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -39,6 +39,7 @@ export const verifyOtp = async (req, res, next) => {
       UserCode: user.UserCode,
       Role: user.Role,
       BranchCode: user.BranchCode,
+      BranchName: user.BranchName,
       AreaCode: user.AreaCode,
       AOCode: user.AOCode
     }
