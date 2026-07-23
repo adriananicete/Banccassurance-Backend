950bb00c41a3304745a5e7ce89125f8c1b9d781f
PL-johndarrylc <phillifedevops@gmail.com>
2026-07-23 09:37:11 +0800

    Added/Removed AO on routes

--- src/routes/referralRoutes.js
+++ src/routes/referralRoutes.js
@@ -11,10 +11,9 @@ import {
   updateReferralStatus,
   getReferralById,
   uploadConsent,
-  getUserNotifications,
-  clearUserNotifications,
-  markNotificationAsRead,
-  markAllNotificationsAsRead
+  // getUserNotifications,
+  // markNotificationAsRead,
+  // markAllNotificationsAsRead
 } from '../controllers/referralController.js'
 import { requireAuth, requireRole } from '../middleware/auth.js'
 import { consentUpload } from '../middleware/upload.js'
